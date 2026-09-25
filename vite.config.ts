d449a6709a826c9fcd3ea564898645499ed52a02
Shree <skumaa2930@gmail.com>
2026-09-25 20:30:39 +0530

    Fix base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/shreya-portfolio/',
+    base: process.env.VERCEL ? '/' : '/shreya-portfolio/',
 
     plugins: [react(), tailwindcss()],
 
